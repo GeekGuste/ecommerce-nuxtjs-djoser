3cced365ccc6b0b7b3c457c46e2c49f905132904
Auguste MANOU <email@email.com>
2022-03-07 20:37:23 +0100

    initialisation du projet

---
+++ hochea_ecommerce_back/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for hochea_ecommerce_back project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hochea_ecommerce_back.settings')
+
+application = get_wsgi_application()
